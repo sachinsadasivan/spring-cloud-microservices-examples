60f5ec6c014401437a844861e99259ab7a31e9ba
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-10-07 13:21:31 +0100

    Code Refactoring with Discovery Server changes

--- product-catalog-service/src/main/java/io/sachinsadasivan/examples/microservices/productcatalogservice/resources/ProductCatalogResource.java
+++ product-catalog-service/src/main/java/io/sachinsadasivan/examples/microservices/productcatalogservice/resources/ProductCatalogResource.java
@@ -1,9 +1,13 @@
 package io.sachinsadasivan.examples.microservices.productcatalogservice.resources;
 
 import io.sachinsadasivan.examples.microservices.productcatalogservice.model.ProductCatalog;
+import io.sachinsadasivan.examples.microservices.productcatalogservice.model.ProductInfo;
+import io.sachinsadasivan.examples.microservices.productcatalogservice.model.ProductPaymentInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/product-catalog")
@@ -11,7 +15,11 @@ public class ProductCatalogResource {
 
     @RequestMapping("/products/{productId}")
     public ProductCatalog getProductCatalog(@PathVariable("productId") String productId){
-        return new ProductCatalog(productId, "Apple iPhone", "Visa");
+        
+        ProductInfo productInfo =  restTemplate.getForObject("http://product-info-service/product-info/" + productId, ProductInfo.class);
+        ProductPaymentInfo productPaymentInfo = restTemplate.getForObject("http://product-payment-service/product-payment-info/" + productId, ProductPaymentInfo.class);
+        
+        return new ProductCatalog(productId, productInfo, productPaymentInfo);
     }
 
 }
