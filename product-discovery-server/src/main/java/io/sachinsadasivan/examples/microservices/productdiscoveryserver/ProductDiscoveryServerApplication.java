bbf3462fb9c43b762a1a296b44a8eaccd8a0968b
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-10-09 15:11:26 +0100

    Added the library for EnableEurekaServer

--- product-discovery-server/src/main/java/io/sachinsadasivan/examples/microservices/productdiscoveryserver/ProductDiscoveryServerApplication.java
+++ product-discovery-server/src/main/java/io/sachinsadasivan/examples/microservices/productdiscoveryserver/ProductDiscoveryServerApplication.java
@@ -2,6 +2,7 @@ package io.sachinsadasivan.examples.microservices.productdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
