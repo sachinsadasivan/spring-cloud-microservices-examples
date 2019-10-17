3328bc2a0a6ae8547216f4c3c3a3e935ea485c27
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-10-17 14:14:14 +0100

    Updated java.util.Optional import

--- product-payment-service/src/main/java/io/sachinsadasivan/examples/microservices/productpaymentservice/resources/ProductPaymentResource.java
+++ product-payment-service/src/main/java/io/sachinsadasivan/examples/microservices/productpaymentservice/resources/ProductPaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product-payment")
