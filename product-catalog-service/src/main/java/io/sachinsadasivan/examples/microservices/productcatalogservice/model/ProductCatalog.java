8b7fdd757ddd8334317cb31991020221c371e35f
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-10-17 15:24:40 +0100

    Update ProductCatalog.java

--- product-catalog-service/src/main/java/io/sachinsadasivan/examples/microservices/productcatalogservice/model/ProductCatalog.java
+++ product-catalog-service/src/main/java/io/sachinsadasivan/examples/microservices/productcatalogservice/model/ProductCatalog.java
@@ -1,15 +1,17 @@
 package io.sachinsadasivan.examples.microservices.productcatalogservice.model;
 
+import io.sachinsadasivan.examples.microservices.productinfoservice.model.ProductPaymentInfo;
+
 public class ProductCatalog {
 
     private String productId;
-    private String productInfo;
-    private String productPaymentInfo;
+    private ProductInfo productInfo;
+    private ProductPaymentInfo productPaymentInfo;
 
     public ProductCatalog() {
     }
 
-    public ProductCatalog(String productId, String productInfo, String productPaymentInfo) {
+    public ProductCatalog(String productId, ProductInfo productInfo, ProductPaymentInfo productPaymentInfo) {
         this.productId = productId;
         this.productInfo = productInfo;
         this.productPaymentInfo = productPaymentInfo;
@@ -23,19 +25,19 @@ public class ProductCatalog {
         this.productId = productId;
     }
 
-    public String getProductInfo() {
+    public ProductInfo getProductInfo() {
         return productInfo;
     }
 
-    public void setProductInfo(String productInfo) {
+    public void setProductInfo(ProductInfo productInfo) {
         this.productInfo = productInfo;
     }
 
-    public String getProductPaymentInfo() {
+    public ProductPaymentInfo getProductPaymentInfo() {
         return productPaymentInfo;
     }
 
-    public void setProductPaymentInfo(String productPaymentInfo) {
+    public void setProductPaymentInfo(ProductPaymentInfo productPaymentInfo) {
         this.productPaymentInfo = productPaymentInfo;
     }
 }
