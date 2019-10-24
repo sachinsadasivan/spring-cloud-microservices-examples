b1aaaeb34bb7c9e62312c8c64ab6b782082f37d7
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-10-24 17:00:33 +0100

    Updated failing Unit Tests

--- product-catalog-service/src/test/java/io/sachinsadasivan/microservices/examples/productcatalogservice/ProductCatalogServiceApplicationTests.java
+++ product-catalog-service/src/test/java/io/sachinsadasivan/microservices/examples/productcatalogservice/ProductCatalogServiceApplicationTests.java
@@ -1,13 +1,14 @@
 package io.sachinsadasivan.microservices.examples.productcatalogservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
-class ProductCatalogServiceApplicationTests {
+public class ProductDiscoveryServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
