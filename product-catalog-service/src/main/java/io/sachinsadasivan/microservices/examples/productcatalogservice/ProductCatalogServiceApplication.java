0cf354e190b30e65af49343c1d1a868116032311
sachinsadasivan <sachin.sadasivan@gmail.com>
2019-11-12 22:55:13 +0000

    Updated with Fault Tolarence with CircuitBreaker

--- product-catalog-service/src/main/java/io/sachinsadasivan/microservices/examples/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/io/sachinsadasivan/microservices/examples/productcatalogservice/ProductCatalogServiceApplication.java
@@ -2,13 +2,16 @@ package io.sachinsadasivan.microservices.examples.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,12 +21,11 @@ public class ProductCatalogServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate(){
-		
 		// Fault Tolerance with Time Out
 		// HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
 		// clientHttpRequestFactory.setConnectionRequestTimeout(3000);
 		// return new RestTemplate(clientHttpRequestFactory);
-		
+
 		return new RestTemplate();
 	}
 }
